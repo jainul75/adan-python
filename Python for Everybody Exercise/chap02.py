e3a8215aad30d3e75567544c0d67f780a24664ac
Ahmad Jainul Abidin <145163754+jainul75@users.noreply.github.com>
2023-09-20 03:21:35 +0600

    Update chap02.py

--- Python for Everybody Exercise/chap02.py
+++ Python for Everybody Exercise/chap02.py
@@ -1,1 +1,32 @@
+# Exercise02
+name = input('Enter your name: ')
+print('Hello', name)
 
+# Exercise03
+a = input('Enter Hours: ')
+b = input('Enter Rate: ')
+c = float(a)*float(b)
+print('Pay:', c)
+
+# Exercise04
+width = 17
+height = 12.0
+v1 =width//2
+v2 = width/2.0
+v3 = height/3
+print(v1)
+print(v2)
+print(v3)
+type(v1)
+type(v2)
+type(v3)
+
+# Exercise05
+# Celsius to Fahrenheit
+temp = input('Celsius: ')
+Fahrenheit = ((9/5)*float(temp))+32
+print('Temperature in fahrenheit is:', Fahrenheit)
+# Fahrenheit to Celsius
+temp = input('Fahrenheit: ')
+Celsius = ((5/9)*(float(temp)-32))
+print('Temperature in celsius is:', Celsius)
