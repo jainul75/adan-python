db3653c683cb37518762a3f6478db153ca87ac3f
Ahmad Jainul Abidin <145163754+jainul75@users.noreply.github.com>
2023-10-05 00:06:06 +0600

    Update chap05.py
    Add exercise questions.

--- Python for Everybody Exercise/chap05.py
+++ Python for Everybody Exercise/chap05.py
@@ -1,4 +1,8 @@
 # exercise 5.1
+"""
+Write a program which repeatedly reads numbers until the user enters “done”. Once “done” is entered, print out the total, count, and average of the
+numbers. If the user enters anything other than a number, detect their mistake using try and except and print an error message and skip to the next number.
+"""
 # initialize the variables with 0 value
 count = 0
 total = 0
@@ -21,6 +25,10 @@ average = total / count
 print('avg:', average)
 
 # exercise 5.2
+"""
+Write another program that prompts for a list of numbers as above and at the end 
+prints out both the maximum and minimum of the numbers instead of the average.
+"""
 max_val = None
 min_val = None
 while True:
